4e290050faf651a5e8e508e0194ed1a62e5d05fc
Eike Kettner <eike.kettner@posteo.de>
2017-07-26 22:52:01 +0200

    add myemacs to nix-env

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -1,11 +1,23 @@
 let
-  mypkgs = [ ../.confnix/pkgs/default.nix ../workspace/projects/confnix/pkgs/default.nix ];
+  mypkgs = [ ../../../.confnix/pkgs/default.nix
+             ../../../workspace/projects/confnix/pkgs/default.nix
+           ];
   pkgs = builtins.filter builtins.pathExists mypkgs;
+  emacs =
+    let
+      dotemacs = builtins.filter builtins.pathExists [
+         ../../../.emacs.d/default.nix
+         ../../../workspace/projects/dot-emacs/default.nix
+      ];
+    in
+    p: if (dotemacs == []) then {}
+       else { myemacs = (import (builtins.head dotemacs) { pkgs = p; }); };
+
 in
 {
   packageOverrides =
-    if (pkgs == []) then p: p
-    else import (builtins.head pkgs);
+    if (pkgs == []) then emacs
+    else p: (import (builtins.head pkgs) p) // (emacs p);
 
   allowUnfree = true;
 }
