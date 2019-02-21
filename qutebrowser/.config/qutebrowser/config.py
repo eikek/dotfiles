646447884e68f5cf84f4994766557158e74ef9da
Eike Kettner <eike.kettner@posteo.de>
2019-02-22 00:50:58 +0100

    config cleanup

--- qutebrowser/.config/qutebrowser/config.py
+++ qutebrowser/.config/qutebrowser/config.py
@@ -36,6 +36,12 @@ c.input.insert_mode.auto_leave = False
 c.new_instance_open_target = 'tab'
 
 c.url.default_page = 'https://start.duckduckgo.com/'
+c.url.searchengines = {
+    "DEFAULT": "https://duckduckgo.com/?q={}",
+    "github": "https://github.com/search?q={}",
+    "scala": "https://www.scala-lang.org/api/2.12.8/?search={}",
+    "dict": "https://www.dict.cc/?s={}"
+}
 
 c.window.hide_decoration = True
 
@@ -55,113 +61,119 @@ ESC_BIND = 'clear-keychain ;; search ;; fullscreen --leave'
 c.bindings.default['normal'] = {}
 # Bindings
 c.bindings.commands['normal'] = {
-        # Navigation
-        '<ctrl-v>': 'scroll-page 0 0.5',
-        '<alt-v>': 'scroll-page 0 -0.5',
-        '<ctrl-shift-v>': 'scroll-page 0 1',
-        '<alt-shift-v>': 'scroll-page 0 -1',
-        '<alt-n>': 'tab-next',
-        '<alt-p>': 'tab-prev',
-        '<alt-r>': 'reload',
+    # Navigation
+    '<ctrl-v>': 'scroll-page 0 0.5',
+    '<alt-v>': 'scroll-page 0 -0.5',
+    '<ctrl-shift-v>': 'scroll-page 0 1',
+    '<alt-shift-v>': 'scroll-page 0 -1',
+    '<alt-n>': 'tab-next',
+    '<alt-p>': 'tab-prev',
+    '<alt-r>': 'reload',
 
-        # Zoom
-        '<ctrl++>': 'zoom-in',
-        '<ctrl+->': 'zoom-out',
+    # Zoom
+    '<ctrl++>': 'zoom-in',
+    '<ctrl+->': 'zoom-out',
 
-        # Commands
-        '<alt-x>': 'set-cmd-text :',
-        '<ctrl-x>b': 'set-cmd-text -s :buffer',
-        '<ctrl-x>k': 'close',
-        'q': 'tab-close',
-        '<ctrl-x><ctrl-c>': 'quit',
-        '<ctrl-x><ctrl-f>': 'set-cmd-text -s :open -t',
-        'G': 'set-cmd-text :open {url:pretty}',
-        'g': 'set-cmd-text :open -r ',
+    # Commands
+    '<alt-x>': 'set-cmd-text :',
+    ':': 'set-cmd-text :',
+    '<ctrl-x>b': 'set-cmd-text -s :buffer',
+    '<ctrl-x>k': 'close',
+    'q': 'tab-close',
+    '<ctrl-x><ctrl-c>': 'quit',
+    '<ctrl-x><ctrl-f>': 'set-cmd-text -s :open -t',
+    'G': 'set-cmd-text :open {url:pretty}',
+    'g': 'set-cmd-text -s :open -r ',
+    'yy': 'yank',
+    'yh': 'hint links yank',
+    
+    # searching
+    '<ctrl-s>': 'set-cmd-text /',
+    '<ctrl-r>': 'set-cmd-text ?',
 
-        # searching
-        '<ctrl-s>': 'set-cmd-text /',
-        '<ctrl-r>': 'set-cmd-text ?',
+    # hinting
+    'f': 'hint',
+    't': 'hint inputs',
+#    ';I': 'hint images tab',
+    ';O': 'hint links fill :open -t -r {hint-url}',
+#    ';R': 'hint --rapid links window',
+#    ';Y': 'hint links yank-primary',
+#    ';b': 'hint all tab-bg',
+    ';d': 'hint links download',
+#    ';f': 'hint all tab-fg',
+    ';h': 'hint all hover',
+    ';i': 'hint images',
+    ';o': 'hint links fill :open {hint-url}',
+    ';r': 'hint --rapid links tab-bg',
 
-        # hinting
-        'f': 'hint',
-        ';I': 'hint images tab',
-        ';O': 'hint links fill :open -t -r {hint-url}',
-        ';R': 'hint --rapid links window',
-        ';Y': 'hint links yank-primary',
-        ';b': 'hint all tab-bg',
-        ';d': 'hint links download',
-        ';f': 'hint all tab-fg',
-        ';h': 'hint all hover',
-        ';i': 'hint images',
-        ';o': 'hint links fill :open {hint-url}',
-        ';r': 'hint --rapid links tab-bg',
-        ';t': 'hint inputs',
-        ';y': 'hint links yank',
+    # history
+    '<ctrl-]>': 'forward',
+    '<ctrl-[>': 'back',
+    'l': 'back',
+    'B': 'back',
+    'F': 'forward',
 
-        # history
-        '<ctrl-]>': 'forward',
-        '<ctrl-[>': 'back',
-        'l': 'back',
-        'B': 'back',
-        'F': 'forward',
+    # editing
+    '<ctrl-f>': 'fake-key <Right>',
+    '<ctrl-b>': 'fake-key <Left>',
+    '<': 'fake-key <Home>',
+    '>': 'fake-key <End>',
+    '<ctrl-n>': 'fake-key <Down>',
+    '<ctrl-p>': 'fake-key <Up>',
+    '<alt-f>': 'fake-key <Ctrl-Right>',
+    '<alt-b>': 'fake-key <Ctrl-Left>',
+    '<ctrl-d>': 'fake-key <Delete>',
+    '<alt-d>': 'fake-key <Ctrl-Delete>',
+    '<alt-backspace>': 'fake-key <Ctrl-Backspace>',
+    '<ctrl-w>': 'fake-key <Ctrl-backspace>',
+    '<ctrl-y>': 'insert-text {clipboard}',
+    '<ctrl-e>': 'enter-mode insert',
 
-        # editing
-        '<ctrl-f>': 'fake-key <Right>',
-        '<ctrl-b>': 'fake-key <Left>',
-        '<': 'fake-key <Home>',
-        '>': 'fake-key <End>',
-        '<ctrl-n>': 'fake-key <Down>',
-        '<ctrl-p>': 'fake-key <Up>',
-        '<alt-f>': 'fake-key <Ctrl-Right>',
-        '<alt-b>': 'fake-key <Ctrl-Left>',
-        '<ctrl-d>': 'fake-key <Delete>',
-        '<alt-d>': 'fake-key <Ctrl-Delete>',
-        '<alt-backspace>': 'fake-key <Ctrl-Backspace>',
-        '<ctrl-w>': 'fake-key <Ctrl-backspace>',
-        '<ctrl-y>': 'insert-text {primary}',
-        '<ctrl-e>': 'enter-mode insert',
+    # Numbers
+    # https://github.com/qutebrowser/qutebrowser/issues/4213
+    '1': 'fake-key 1',
+    '2': 'fake-key 2',
+    '3': 'fake-key 3',
+    '4': 'fake-key 4',
+    '5': 'fake-key 5',
+    '6': 'fake-key 6',
+    '7': 'fake-key 7',
+    '8': 'fake-key 8',
+    '9': 'fake-key 9',
+    '0': 'fake-key 0',
 
-        # Numbers
-        # https://github.com/qutebrowser/qutebrowser/issues/4213
-        '1': 'fake-key 1',
-        '2': 'fake-key 2',
-        '3': 'fake-key 3',
-        '4': 'fake-key 4',
-        '5': 'fake-key 5',
-        '6': 'fake-key 6',
-        '7': 'fake-key 7',
-        '8': 'fake-key 8',
-        '9': 'fake-key 9',
-        '0': 'fake-key 0',
-
-        # escape hatch
-        '<ctrl-h>': 'set-cmd-text -s :help',
-        '<ctrl-g>': ESC_BIND,
+    # escape hatch
+    '<ctrl-h>': 'set-cmd-text -s :help',
+    '<ctrl-g>': ESC_BIND,
 }
 
 c.bindings.commands['command'] = {
-        '<ctrl-s>': 'search-next',
-        '<ctrl-r>': 'search-prev',
+    '<ctrl-s>': 'search-next',
+    '<ctrl-r>': 'search-prev',
 
-        '<ctrl-p>': 'completion-item-focus prev',
-        '<ctrl-n>': 'completion-item-focus next',
+    '<ctrl-p>': 'completion-item-focus prev',
+    '<ctrl-n>': 'completion-item-focus next',
 
-        '<alt-p>': 'command-history-prev',
-        '<alt-n>': 'command-history-next',
+    '<alt-p>': 'command-history-prev',
+    '<alt-n>': 'command-history-next',
 
-        # escape hatch
-        '<ctrl-g>': 'leave-mode',
+    # escape hatch
+    '<ctrl-g>': 'leave-mode',
 }
 
 c.bindings.commands['hint'] = {
-        # escape hatch
-        '<ctrl-g>': 'leave-mode',
+    # escape hatch
+    '<ctrl-g>': 'leave-mode',
 }
 
 
 c.bindings.commands['caret'] = {
-        # escape hatch
-        '<ctrl-g>': 'leave-mode',
+    # escape hatch
+    '<ctrl-g>': 'leave-mode',
+}
+
+c.bindings.commands['insert'] = {
+    '<ctrl-y>': 'insert-text {clipboard}',
 }
 
 if socket.gethostname() == "kythira":
