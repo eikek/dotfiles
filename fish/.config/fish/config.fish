0b05fd01444f685250cbbfc6d3fc3b2214202a79
Eike Kettner <eike.kettner@posteo.de>
2019-08-29 17:14:54 +0200

    fish: add basic reverse search

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,3 +7,16 @@ alias playr="mpv (find . -type f | perl -MList::Util=shuffle -e 'print shuffle(<
 alias mux='tmuxinator'
 alias mpvu='mpv --audio-device=alsa/iec958:CARD=X5,DEV=0'
 alias wallpaper='feh --bg-fill -z ~/.backgrounds/'
+
+
+# from here: https://github.com/fish-shell/fish-shell/issues/602
+function reverse_history_search
+  history | fzf --no-sort --height 50% --layout reverse | read -l command
+  if test $command
+    commandline -rb $command
+  end
+end
+
+function fish_user_key_bindings
+  bind \cr reverse_history_search
+end
