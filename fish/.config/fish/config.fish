303a94a3059fd048078dc804ff99afb01deb84cc
Eike Kettner <eike.kettner@posteo.de>
2019-08-29 09:44:02 +0200

    Fix fish init

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,7 +3,7 @@
 alias l='exa -lha --git'
 alias cat='bat'
 alias mc='mc --colors="normal=white,black:header=white,red:menunormal=white,color90"'
-alias playr="mpv \"\$(find . -type f | perl -MList::Util=shuffle -e 'print shuffle(<STDIN>);' | tail -n 1)\""
+alias playr="mpv (find . -type f | perl -MList::Util=shuffle -e 'print shuffle(<STDIN>);' | tail -n 1)"
 alias mux='tmuxinator'
 alias mpvu='mpv --audio-device=alsa/iec958:CARD=X5,DEV=0'
 alias wallpaper='feh --bg-fill -z ~/.backgrounds/'
