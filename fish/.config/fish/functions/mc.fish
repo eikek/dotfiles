6d4896d00e8f6b023faa21a8b6320289fe2f17cb
Eike Kettner <eike.kettner@posteo.de>
2019-09-01 23:32:09 +0200

    fish: fix mc alias

--- fish/.config/fish/functions/mc.fish
+++ fish/.config/fish/functions/mc.fish
@@ -1,3 +1,3 @@
 function mc
-    command mc --colors="normal=white,black:header=white,red:menunormal=white,color90"
+    command mc --colors="normal=white,black:header=white,red:menunormal=white,color90" $argv
 end
