ee7adcf130ea40a03794701727f0a85fd2908cef
Eike Kettner <eike.kettner@posteo.de>
2020-04-26 00:27:42 +0200

    Remove unused stuff

--- lein/.lein/profiles.clj
+++
@@ -1,7 +0,0 @@
-{:user
- {:plugins [[lein-exec "0.3.6"]
-            [com.aphyr/prism "0.1.2"]]
-  :dependencies [[com.aphyr/prism "0.1.2"]
-                 [pjstadig/humane-test-output "0.8.1"]]
-  :injections [(require 'pjstadig.humane-test-output)
-               (pjstadig.humane-test-output/activate!)]}}
