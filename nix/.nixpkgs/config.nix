17f9f787113dd659267effd3183eb2b5af823689
Eike Kettner <eike.kettner@posteo.de>
2020-05-02 11:58:45 +0200

    Remove emacs from nix-env

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -3,21 +3,10 @@ let
              ../../../workspace/projects/confnix/pkgs/default.nix
            ];
   pkgs = builtins.filter builtins.pathExists mypkgs;
-  emacs =
-    let
-      dotemacs = builtins.filter builtins.pathExists [
-         ../../../.emacs.d/default.nix
-         ../../../workspace/projects/dot-emacs/default.nix
-      ];
-    in
-    p: if (dotemacs == []) then {}
-       else { myemacs = (import (builtins.head dotemacs) { pkgs = p; }); };
-
 in
 {
   packageOverrides =
-    if (pkgs == []) then emacs
-    else p: (import (builtins.head pkgs) p) // (emacs p);
+    p: (import (builtins.head pkgs) p);
 
   allowUnfree = true;
 }
