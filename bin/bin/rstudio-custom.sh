f5e356622bbff63acd09ef2386fff7742bb5ecc6
Eike Kettner <eike.kettner@posteo.de>
2021-04-16 01:06:57 +0200

    rstudio package

--- bin/bin/rstudio-custom.sh
+++ bin/bin/rstudio-custom.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-nix-shell --packages 'rstudioWrapper.override{ packages = with rPackages; [  ggplot2 dplyr xts rmarkdown evaluate digest highr markdown stringr yaml Rcpp htmltools knitr jsonlite base64enc mime ]; }'
+nix-shell --packages 'rstudioWrapper.override{ packages = with rPackages; [ tidyr ggplot2 dplyr xts rmarkdown evaluate digest highr markdown stringr yaml Rcpp htmltools knitr jsonlite base64enc mime ]; }' --run rstudio
