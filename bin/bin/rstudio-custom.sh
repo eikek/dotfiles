f6556b815179ac8eb21f9948ebae983037c86fee
Eike Kettner <eike.kettner@posteo.de>
2021-04-28 22:44:01 +0200

    Fix alacritty, add packages to rstudio

--- bin/bin/rstudio-custom.sh
+++ bin/bin/rstudio-custom.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-nix-shell --packages 'rstudioWrapper.override{ packages = with rPackages; [ tidyr ggplot2 dplyr xts rmarkdown evaluate digest highr markdown stringr yaml Rcpp htmltools knitr jsonlite base64enc mime ]; }' --run rstudio
+nix-shell --packages 'rstudioWrapper.override{ packages = with rPackages; [ tidyr ggplot2 dplyr xts rmarkdown evaluate digest highr markdown stringr yaml Rcpp htmltools knitr jsonlite base64enc mime lmtest tidyverse reshape2 broom ]; }' --run rstudio
