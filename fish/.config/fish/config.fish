cbe042e53da9752530bd71033d85b8505f747bec
eikek <eike.kettner@posteo.de>
2021-10-31 22:43:03 +0100

    Load some settings conditionally

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -18,21 +18,9 @@ function fish_vterm_prompt_end;
 end
 function track_directories --on-event fish_prompt; fish_vterm_prompt_end; end
 
-set -gx NIX_PATH "$HOME/.nix-defexpr/channels"
-set -gx NIX_PROFILES "/nix/var/nix/profiles/default $HOME/.nix-profile"
-set -gx NIX_SSL_CERT_FILE "$HOME/.nix-profile/etc/ssl/certs/ca-bundle.crt"
-set -gx MANPATH "$HOME/.nix-profile/share/man" $MANPATH
-set -xg SSH_AUTH_SOCK /Users/eike/.gnupg/S.gpg-agent.ssh
+if test (hostname) = "Eikes-MBP.home"
+    source $HOME/.config/fish/work.fish
+end
 
-set -gx PATH ~/bin ~/.nix-profile/bin $PATH
-
-dsc generate-completions --shell fish | source
-
-# set -gx AWS_ACCESS_KEY_ID (gopass tundra/aws/security/eike.kettner/aws-access-key)
-# set -gx AWS_SECRET_ACCESS_KEY (gopass tundra/aws/security/eike.kettner/aws-secret-key)
-set -gx SBT_OPTS "-Xms512M -Xmx4G -Xss32M -Duser.timezone=GMT"
-set -gx AWS_DEFAULT_REGION eu-west-1
-set -gx AWS_HOME /home/(whoami)/.aws
-set -gx AWS_REGION eu-west-1
 
 starship init fish | source
