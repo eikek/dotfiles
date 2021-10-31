f9005b7c94219bf257069ea4ae9a17c93ca59b3b
Eike Kettner <eike.kettner@posteo.de>
2021-10-31 23:20:02 +0100

    Add homebrew to PATH

--- fish/.config/fish/work.fish
+++ fish/.config/fish/work.fish
@@ -4,12 +4,12 @@ set -gx NIX_SSL_CERT_FILE "$HOME/.nix-profile/etc/ssl/certs/ca-bundle.crt"
 set -gx MANPATH "$HOME/.nix-profile/share/man" $MANPATH
 set -xg SSH_AUTH_SOCK /Users/eike/.gnupg/S.gpg-agent.ssh
 
-set -gx PATH ~/bin ~/.nix-profile/bin $PATH
+set -gx PATH ~/bin ~/.nix-profile/bin /opt/homebrew/bin $PATH
 
 dsc generate-completions --shell fish | source
 
-# set -gx AWS_ACCESS_KEY_ID (gopass tundra/aws/security/eike.kettner/aws-access-key)
-# set -gx AWS_SECRET_ACCESS_KEY (gopass tundra/aws/security/eike.kettner/aws-secret-key)
+# set -gx AWS_ACCESS_KEY_ID (pass tundra/aws/security/eike.kettner/aws-access-key)
+# set -gx AWS_SECRET_ACCESS_KEY (pass tundra/aws/security/eike.kettner/aws-secret-key)
 set -gx SBT_OPTS "-Xms512M -Xmx4G -Xss32M -Duser.timezone=GMT"
 set -gx AWS_DEFAULT_REGION eu-west-1
 set -gx AWS_HOME /home/(whoami)/.aws
