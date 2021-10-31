843ea55f895fe6e7492e4037d753a9abf491df12
Eike Kettner <eike.kettner@tundra.com>
2021-10-31 21:37:36 +0100

    Update env vars

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -20,4 +20,11 @@ function track_directories --on-event fish_prompt; fish_vterm_prompt_end; end
 
 set -gx PATH ~/bin $PATH
 
+# set -gx AWS_ACCESS_KEY_ID (gopass tundra/aws/security/eike.kettner/aws-access-key)
+# set -gx AWS_SECRET_ACCESS_KEY (gopass tundra/aws/security/eike.kettner/aws-secret-key)
+set -gx SBT_OPTS "-Xms512M -Xmx4G -Xss32M -XX:+CMSClassUnloadingEnabled -Duser.timezone=GMT -Dsqlite4java.library.path=services/buy/core/src/test/lib"
+set -gx AWS_DEFAULT_REGION eu-west-1
+set -gx AWS_HOME /home/(whoami)/.aws
+set -gx AWS_REGION eu-west-1
+
 starship init fish | source
