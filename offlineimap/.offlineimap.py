9014e60fca763ae21b53d8438f736bc0abd13227
eikek <eike.kettner@posteo.de>
2021-12-20 19:03:45 +0100

    Fix offlineimap for 8.0.0 (nixos 21.11)

--- offlineimap/.offlineimap.py
+++ offlineimap/.offlineimap.py
@@ -2,14 +2,14 @@
 import subprocess
 
 def call_pass(name):
-    p = subprocess.Popen(["pass", "show", name], stdout=subprocess.PIPE)
-    return p.stdout.read()
+    p = subprocess.run(["pass", "show", name], stdout=subprocess.PIPE)
+    return p.stdout.decode('utf-8')
 
 def get_password_pass(name):
-    return call_pass(name).split('\n', 1)[0]
+    return call_pass(name).splitlines()[0]
 
 def get_user_pass(name):
-    return findValue('user', call_pass(name).split('\n'))
+    return findValue('user', call_pass(name).splitlines())
 
 def byKey(key):
     return lambda x: x.startswith(key + ':')
@@ -18,10 +18,10 @@ def getValue(key):
     return lambda x: x.lstrip(key +':').strip()
 
 def findValue(key, lines):
-    return map(getValue(key), filter(byKey(key), lines))[0]
+    return list(map(getValue(key), filter(byKey(key), lines)))[0]
 
 def get_host_pass(name):
-    return findValue('mailhost', call_pass(name).split('\n'))
+    return findValue('mailhost', call_pass(name).splitlines())
 
 def get_port_pass(name):
-    return int(findValue('imap-port', call_pass(name).split('\n')))
+    return int(findValue('imap-port', call_pass(name).splitlines()))
