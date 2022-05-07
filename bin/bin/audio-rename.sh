2f92ffc8b4f67129a167ef7d3d69e180f8806464
eikek <eike.kettner@posteo.de>
2022-05-07 10:14:24 +0200

    update

--- bin/bin/audio-rename.sh
+++ bin/bin/audio-rename.sh
@@ -6,14 +6,21 @@ if [ "$1" = "--dry" ]; then
     shift
 fi
 
-GOOD='a-zA-z0-9 _\-.,:'
+GOOD='a-zA-z0-9 _\-.,:öäüÖÄÜ'
 for f in "$@"; do
     ext="${f##*.}"
-    name=$(mediainfo --Inform="General;%Track/Position%-%Title%.$ext" "$f" | \
+    name=$(mediainfo --Inform="General;%Track/Position%-%Composer%-%Title%.$ext" "$f" | \
                tr 'è' 'e' | \
                tr 'È' 'E' | \
                tr 'à' 'a' | \
                tr 'À' 'A' | \
+               tr 'ß' "ss" | \
+               tr 'ü' "ue" | \
+               tr 'Ü' "Ue" | \
+               tr 'ä' "ae" | \
+               tr 'Ä' "Ae" | \
+               tr 'ö' "oe" | \
+               tr 'Ö' "Oe" | \
                tr -d -c "$GOOD" | tr ':' ',')
     if [ $dry -eq 1 ]; then
         echo "$f --> $name"
