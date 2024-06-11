76a53fd6d4e60a09bd0409d1737991ae5c449d28
eikek <eike.kettner@posteo.de>
2024-06-11 08:30:58 +0200

    update

--- fish/.config/fish/functions/playr.fish
+++ fish/.config/fish/functions/playr.fish
@@ -1,5 +1,5 @@
 function playr
     set -l file (find . -type f -iname "*.mkv" -or -iname "*.avi" -or -iname "*.mpg" -or -iname "*.mpeg" -or -iname "*.mp4" | perl -MList::Util=shuffle -e 'print shuffle(<STDIN>);' | tail -n 1)
     echo $file
-    mpv $file
+    mpv --volume=0 $file
 end
